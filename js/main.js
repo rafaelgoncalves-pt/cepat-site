6435cb1ab330491d4ae125ce828ab4e5025429e4
Pedro Meireis <pedromeireis@esad.pt>
2022-09-12 22:10:52 +0100

    Improve styling

--- js/main.js
+++ js/main.js
@@ -5,9 +5,6 @@ const project = document.querySelector('.page-project')
 window.addEventListener('scroll', () => {
     if(intro.getBoundingClientRect().top <= header.offsetHeight) {
         header.classList.add('page')
-        console.log(project.getBoundingClientRect().top)
-        console.log(project.getBoundingClientRect().height)
-        console.log(header.offsetHeight)
         if (project.getBoundingClientRect().top <= header.offsetHeight && project.getBoundingClientRect().top > project.getBoundingClientRect().height * -1) {
             header.classList.add('project')
         } else {
