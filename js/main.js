ea8ddc2e79d382c3235218fe5ef5116cdc2b0c76
Rafael Gonçalves <55799311+rafaelgoncalves-pt@users.noreply.github.com>
2023-03-07 09:43:12 +0000

    Website Launch

--- js/main.js
+++ js/main.js
@@ -95,3 +95,22 @@ projectAside.forEach(text => {
         }
     });
 });
+
+gsap.utils.toArray(".comparison-section").forEach(section => {
+    let tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: section,
+            start: "center center",
+            // makes the height of the scrolling (while pinning) match the width, thus the speed remains constant (vertical/horizontal)
+            end: () => "+=" + section.offsetWidth,
+            scrub: true,
+            pin: true,
+            anticipatePin: 1
+        },
+        defaults: { ease: "none" }
+    });
+    // animate the container one way...
+    tl.fromTo(section.querySelector(".after-image"), { xPercent: 100, x: 0 }, { xPercent: 0 })
+        // ...and the image the opposite way (at the same time)
+        .fromTo(section.querySelector(".after-image img"), { xPercent: -100, x: 0 }, { xPercent: 0 }, 0);
+});
